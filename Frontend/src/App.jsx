d71030db0e5407d2f9396d51ccae636446c3ec45
Philip Eisner <pjeisner@wisc.edu>
2025-02-18 13:34:26 -0600

    added bootstrap as dependency and added a test button to frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
    <div>
-
+    <button
+      title="Test Button"
+      color="#841584"
+      
+>Test Button</button>
    </div>
   )
 }
