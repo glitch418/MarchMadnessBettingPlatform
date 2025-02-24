d1d57fa4832bb3710ff79fce4a5e4744655b04ae
Tejas Gupta <tgupta39@wisc.edu>
2025-02-24 14:01:39 -0600

    Adding routes functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Game from './components/Game/Game';
+
 
 const App = () => {
   return (
-   <div>
-    <button
-      title="Test Button"
-      color="#841584"
-      
->Test Button</button>
-   </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        {/* TODO: Sign up page */}
+
+        {/* TODO: Protect routes based on login status */}
+        <Route path="/" element={<Home />} />
+
+        {/* Any undefined paths redirect to login */}
+        <Route path="*" element={<Login/>} />
+      </Routes>
+  </Router>
   )
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+    return (
+     <div>
+        <p>This is Login</p>
+     </div>
+    )
+  }
+  
+  export default Login;
