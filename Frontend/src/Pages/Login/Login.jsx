04cfe17741d06deb1b64040d487439e6a649d8cd
Tejas Gupta <tgupta39@wisc.edu>
2025-02-24 14:15:49 -0600

    Adding login

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
     return (
      <div>
-        <p>This is Login</p>
+        <form onSubmit={() => (null)}>
+            <input placeholder="example@email.com" onChange={(e) => setEmail(e.target.value)}/>
+            <input type={showPassword ? "text" : "password"} placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+            <div>
+            <input onChange={() => setShowPassword(!showPassword)} type="checkbox"/>
+            <p> Show Password</p>
+            </div>
+        </form>
      </div>
     )
   }
