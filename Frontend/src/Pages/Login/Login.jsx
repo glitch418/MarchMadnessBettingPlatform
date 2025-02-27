4f4f98352be11e3581911e63328e39f9379835ec
Tejas Gupta <tgupta39@wisc.edu>
2025-02-26 19:47:01 -0600

    Added sign in navigation and changes to UI

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        //TODO: check credentials
+        if (true){
+            navigate("/");
+        }
+    }
+
     return (
-     <div>
-        <form onSubmit={() => (null)}>
-            <input placeholder="example@email.com" onChange={(e) => setEmail(e.target.value)}/>
-            <input type={showPassword ? "text" : "password"} placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
-            <div>
-            <input onChange={() => setShowPassword(!showPassword)} type="checkbox"/>
-            <p> Show Password</p>
-            </div>
-        </form>
-     </div>
+        <div>
+            {/*TODO: update handle submit */}
+            <form onSubmit={handleSubmit}>
+                <input placeholder="example@email.com" onChange={(e) => setEmail(e.target.value)}/>
+                <input type={showPassword ? "text" : "password"} placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+                <div><input onChange={() => setShowPassword(!showPassword)} type="checkbox"/> <p> Show Password</p></div>  
+                <button type="submit" disabled={!email.trim() || !password.trim()}>Sign in</button>
+            </form>
+
+            {/*TODO: handle create account */}
+            <button onClick={() => (null)} type="button">Create Account</button>
+
+            {/*TODO: handle forgot password */}
+            <button type="button" onClick={() => (null)}>Forgot Password?</button>
+        </div>
     )
-  }
+}
   
   export default Login;
