941f8b53b6c63fff8e8d558aba476dd3cfa3a517
Preston Wray <pdwray@wisc.edu>
2025-03-05 22:24:13 -0600

    Line generator v2 with improved predictor and betting line comparison test

--- 2024_game_results/GamePredictor.py
+++ 2024_game_results/GamePredictor.py
@@ -1,32 +1,26 @@
 import pandas as pd
-import numpy as np
-import math
 
-# 1. Read in from the csv
+# 1. Read in the csv with each team's advanced statistics using pandas
 stats = pd.read_csv('./2024_Team_Adv_Stats.csv')
-stats
+#stats
+# To get and print stats about a team(ex Illinois)
+# team_a = "Illinois"
+# stats_needed = loaded.loc[0:6, team_a]
+# for param, value in zip(stats_needed.index, stats_needed.values):
+#     print(f"{loaded.loc[param, 'Parameter']}: {value}")
 
-# 2. Get Impact Factor Coefficients
-alpha = 0.5 # Seed Factor
-beta = 0.75 # Win% * SOS/10
-gamma = 1.7 # ORtg - Opp DRtg
-delta = 1.75 # DRtg - Opp ORtg
-epsilon = 1.5 # Adj Efficiency diff
-zeta = -0.35 # Tempo difference
 
-#team_a = "Illinois"
-#stats_needed = loaded.loc[0:6, team_a]
-#for param, value in zip(stats_needed.index, stats_needed.values):
-#    print(f"{loaded.loc[param, 'Parameter']}: {value}")
-
-# 3. Get Team statistics by selected team
-# team a (Higher seed(or equal))
-team_a = "Washington St"
-team_a_stats = stats.loc[0:6, team_a]
-# team b (Lower seed(or equal))
-team_b = "Drake"
-team_b_stats = stats.loc[0:6, team_b]
+# 2. Select 2 teams to collect advanced statistics for, team a and teamb, using loc & iloc
+# 
+# for entering teams, first letter each word capitalized, State is St for all except NC State,
+# A&M stays as is, saint is St for peters and marys 
+#
+# higher or equal seed: team a
+team_a = "UConn"
+# lower or equal seed: team b
+team_b = "Purdue" 
 # team a stats
+team_a_stats = stats.loc[0:6, team_a]
 seed_a = team_a_stats.iloc[0]
 winp_a = team_a_stats.iloc[1]
 sos_a = team_a_stats.iloc[2]
@@ -35,6 +29,7 @@ drtg_a = team_a_stats.iloc[4]
 nrtg_a = team_a_stats.iloc[5]
 adjt_a = team_a_stats.iloc[6]
 # team b stats
+team_b_stats = stats.loc[0:6, team_b]
 seed_b = team_b_stats.iloc[0]
 winp_b = team_b_stats.iloc[1]
 sos_b = team_b_stats.iloc[2]
@@ -43,19 +38,33 @@ drtg_b = team_b_stats.iloc[4]
 nrtg_b = team_b_stats.iloc[5]
 adjt_b = team_b_stats.iloc[6]
 
+
+# 3. Get Impact Factor Coefficients; This will represent how much each element will impact the regression
+alpha = [0.5,0.75,0.75,1.0,0.5] # Seed Factor
+beta = [0.75,1.0,1.0,1.0,0.5] # Win% * SOS/10
+gamma = [1.7,1.0,1.5,1.0,0.25] # ORtg - Opp DRtg (projected points scored)
+delta = [1.75,1.0,1.55,1.0,0.25] # DRtg - Opp ORtg (projected points allowed)
+epsilon = [1.5,1.75,1.75,1.0,1.5] # Adj Efficiency diff
+zeta = [-0.35,-0.25,-0.25,-0.25,0] # Tempo difference
+
+
 # 4. Use statistical model to predict winner of game based on stats and impact factor coefficients
+#    To avoid error in one model, we will use 4 different combinations of factors and average them
+#    We get the "line"(how much the favored team is expected to win by) by dividing the avg zscore by 4
 seed_a_adjusted = 17 - seed_a
 seed_b_adjusted = 17 - seed_b
-zscore = (seed_a_adjusted-seed_b_adjusted)*alpha + ((winp_a*sos_a)-(winp_b*sos_b))*beta + (ortg_a-drtg_b)*gamma + (drtg_a-ortg_b)*delta + (nrtg_a-nrtg_b)*epsilon + (adjt_a-adjt_b)*zeta
-winprob_a = (1.0 / (1 + math.exp(-zscore)))*100
-winprob_b = (1 - (winprob_a/100))*100
-print(zscore)
-odds_winner = abs(zscore) / 4
-if(zscore>0):
-    print(f"{team_a} is favored to win by {round(odds_winner)} points")
-elif(zscore<0):
-    print(f"{team_b} is favored to win by {round(odds_winner)} points")
+zscore_total = 0
+for i in range(5):
+    zscore = (seed_a_adjusted-seed_b_adjusted)*alpha[i] + ((winp_a*sos_a)-(winp_b*sos_b))*beta[i] + \
+        (ortg_a-drtg_b)*gamma[i] + (drtg_a-ortg_b)*delta[i] + (nrtg_a-nrtg_b)*epsilon[i] + (adjt_a-adjt_b)*zeta[i]
+    zscore_total += zscore
+    #print(zscore)
+zscore_avg = zscore_total / 5 # 5 total regressions
+odds_winner = round(abs(zscore_avg) / 4) # divide by 4 to get close to real lines and round
+#print(zscore_avg)
+if(zscore_avg>0):
+    print(f"{team_a} is favored to win by {odds_winner} points over {team_b} ({team_a} -{odds_winner})")
+elif(zscore_avg<0):
+    print(f"{team_b} is favored to win by {odds_winner} points over {team_a} ({team_b} -{odds_winner})")
 else:
     print(f"{team_a} and {team_b} have even odds to win")
-#print(f"{team_a} has a {winprob_a:.2f}% chance of winning the game")
-#print(f"{team_b} has a {winprob_b:.2f}% chance of winning the game")
