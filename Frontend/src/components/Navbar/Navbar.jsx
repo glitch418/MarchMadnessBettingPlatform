aa9c47b74379b1f1119a262b46d224d4eb02c1e7
Tejas Gupta <tgupta39@wisc.edu>
2025-04-10 18:35:55 -0500

    Fixed navbar styling to include balance

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import bet from "../../assets/bet.png";
 import search from "../../assets/search.png";
 import profile from "../../assets/profile.png";
 import placeBet from "../../assets/placebet.png";
+import dollarSign from "../../assets/dollar-sign.png"
 import { useUserSession } from "../../contexts/UserSessionContext";
 
 const Navbar = () => {
@@ -46,8 +47,8 @@ const Navbar = () => {
 
     return (
         <nav>
-            {/* Main container */}
-            <div className="container">
+            {/* Left section - Page Navigation*/}
+            <div className="left-section">
 
                 {/* Home */}
                 <div className="nav-item" onClick={handleHomeClick}>
@@ -68,7 +69,7 @@ const Navbar = () => {
                 </div>
             </div>
 
-            {/* Search Bar */}
+            {/* Middle Section - Search Bar */}
             <div className="search-container" onClick={handleSearchSubmit}>
                 <img id="search-icon" src={search} alt="Search" />
                 <input
@@ -81,14 +82,27 @@ const Navbar = () => {
                 />
             </div>
 
-            {/* Login/Profile */}
-            <div
-                className="nav-item"
-                onClick={isLoggedIn ? handleProfileClick : handleLoginClick}
-            >
-                <img id="profile-icon" src={profile} alt="Profile" />
-                <p>{isLoggedIn ? `${userEmail?.charAt(0).toUpperCase()}` : 'Login'}</p>
+            {/* Right section - user info */}
+            <div className="user-section">
+                {/* Login/Profile */}
+                <div
+                    className="nav-item"
+                    onClick={isLoggedIn ? handleProfileClick : handleLoginClick}
+                >
+                    <img id="profile-icon" src={profile} alt="Profile" />
+                    <p>{isLoggedIn ? `${userEmail?.charAt(0).toUpperCase()}` : 'Login'}</p>
+                </div>
+
+                {/* Balance - only displayed when logged in */}
+                {isLoggedIn && userEmail && (
+                    <div className="balance-item" onClick={handleBetsClick}>
+                        <img id="balance-icon" src={dollarSign} alt="Balance" />
+                        <p>{balance}</p>
+                    </div>
+                )}
+                
             </div>
+
         </nav>
     );
 };
