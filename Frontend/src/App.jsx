86cdac79048ad5d5a39d139773c7ee94a817e8b3
Tejas Gupta <tgupta39@wisc.edu>
2025-04-20 15:13:32 -0500

    Navbar on every page through App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './Pages/Profile/Profile';
 import UserSessionProvider from './contexts/UserSessionContext';
 import { GamesProvider } from './contexts/GetGamesContext';
 import { TeamsProvider } from './contexts/GetTeamsContext';
+import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -16,26 +17,29 @@ const App = () => {
       <GamesProvider>
         <UserSessionProvider>
           <Router> {/* Provides routing context */}
-            <Routes> {/* Container that matches routes */}
 
-              {/* Default path directs user to home page */}
-              <Route path="/" element={<Home />} />
+            <Navbar />
+            
+            <div style={{ marginTop: '8vh' }}>
+              <Routes> {/* Container that matches routes */}
 
-              {/* Login Page */}
-              <Route path="/login" element={<Login/>} />
+                {/* Default path directs user to home page */}
+                <Route path="/" element={<Home />} />
 
-              {/* TODO: Profile page */}
-              <Route path="/profile" element={<Profile/>} />
+                {/* Login Page */}
+                <Route path="/login" element={<Login/>} />
+                {/* TODO: Profile page */}
+                <Route path="/profile" element={<Profile/>} />
+                {/* MyBets Page */}
+                <Route path="/myBets" element={<MyBets/>} />
+                {/* PlaceBet Page */}
+                <Route path="/placeBet" element={<PlaceBet />} />
 
-              {/* MyBets Page */}
-              <Route path="/myBets" element={<MyBets/>} />
+                {/* Any undefined paths redirect to Home */}
+                <Route path="*" element={<Home/>} />
+              </Routes>
+            </div>
 
-              {/* PlaceBet Page */}
-              <Route path="/placeBet" element={<PlaceBet />} />
-
-              {/* Any undefined paths redirect to Home */}
-              <Route path="*" element={<Home/>} />
-            </Routes>
           </Router>
         </UserSessionProvider>
       </GamesProvider>
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./Home.css";
-import Navbar from "../../components/Navbar/Navbar";
-import Query from "../../utils/Query";
 import TournamentBracket from "../../components/TournamentBracket/TournamentBracket";
 
 
 const Home = () => {
     return (
      <div>
-         <Navbar/>
-         <div style={{paddingTop: '8vh', display: 'flex', justifyContent: 'center'}}>
-            {/* ENABLE/DISABLE query box by commenting out: */}
-            {/* <Query/> */}
-            <div>
-            <TournamentBracket />
-            </div>
-        </div>
+        <TournamentBracket />
      </div>
     )
   }
